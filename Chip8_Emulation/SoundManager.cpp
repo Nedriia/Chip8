5d6b7a19d2ee12ff41861761e5f31917de0ac90a
Nedriia <arthur.favre@hotmail.com>
2026-07-02 19:20:49 +0200

    fix pitch init

--- Chip8_Emulation/SoundManager.cpp
+++ Chip8_Emulation/SoundManager.cpp
@@ -10,7 +10,6 @@
 #define CHUNK_SIZE			128
 #define AMPLITUDE			0.2f
 
-static ma_audio_buffer	g_oAudioBuffer;
 static ma_waveform		g_oWaveForm;
 static bool				g_bPlaySound = false;
 
@@ -41,7 +40,6 @@ void SoundManager::DestroySoundManager()
 
 		ClearAudioBuffer();
 
-		ma_audio_buffer_uninit( &g_oAudioBuffer );
 		ma_waveform_uninit( &g_oWaveForm );
 
 		ma_device_uninit( &m_oDevice );
@@ -87,7 +85,7 @@ void SoundManager::OnReset()
 {
 	ClearAudioBuffer();
 
-	m_iPitch = SAMPLE_RATE;
+	m_iPitch = 4000;
 	m_fFloatingIndex = 0.0f;
 	m_iAudioStateFlag = AudioState::AUDIO_BUFFER_EMPTY;
 }
@@ -100,23 +98,23 @@ static void data_callback( ma_device* pDevice,void* pOutput,const void* pInput,m
 		SoundManager* pInstance = SoundManager::GetInstance();
 		if( pInstance->GetState() == AudioState::AUDIO_BUFFER_FILLED )
 		{
-		float fValueIncrement = pInstance->GetPitch() / SAMPLE_RATE;
-		float* pData = ( float* )pOutput;
+			float fValueIncrement = pInstance->GetPitch() / SAMPLE_RATE;
+			float* pData = ( float* )pOutput;
 
-		for( ma_uint64 iFrameRead = 0; iFrameRead < frameCount; ++iFrameRead )
-		{
-			float fCurrentIndex = pInstance->GetFloatingIndex();
+			for( ma_uint64 iFrameRead = 0; iFrameRead < frameCount; ++iFrameRead )
+			{
+				float fCurrentIndex = pInstance->GetFloatingIndex();
 
-			pData[iFrameRead] = m_aAudioData[ ( int )fCurrentIndex ];
+				pData[iFrameRead] = m_aAudioData[ ( int )fCurrentIndex ];
 
-			float fNewValue = fCurrentIndex + fValueIncrement;
-			if( fNewValue >= 128.0f )
-				fNewValue -= 128.0f;
-			pInstance->SetFloatingIndex( fNewValue );
-		}
+				float fNewValue = fCurrentIndex + fValueIncrement;
+				if( fNewValue >= 128.0f )
+					fNewValue -= 128.0f;
+				pInstance->SetFloatingIndex( fNewValue );
+			}
 
-			if( g_bPlaySound == false )
-				pInstance->SetState( AudioState::AUDIO_BUFFER_EMPTY );
+				if( g_bPlaySound == false )
+					pInstance->SetState( AudioState::AUDIO_BUFFER_EMPTY );
 		}
 		else
 		{
@@ -141,10 +139,6 @@ void SoundManager::Init()
 	if( ma_waveform_init( &oSquareWaveConfig,&g_oWaveForm ) != MA_SUCCESS )
 		std::cerr << "SOUNDMANAGER::FAILED_TO_INIT_WAVEFORM" << std::endl;
 
-	ma_audio_buffer_config oAudioBufferConfig = ma_audio_buffer_config_init( DEVICE_FORMAT,1,CHUNK_SIZE,m_aAudioData,NULL );
-	if( ma_audio_buffer_init( &oAudioBufferConfig,&g_oAudioBuffer ) != MA_SUCCESS )
-		std::cerr << "SOUNDMANAGER::FAILED_TO_INIT_AUDIO_BUFFER" << std::endl;
-
 	ma_device_config oDeviceConfig = ma_device_config_init( ma_device_type_playback );
 					oDeviceConfig.playback.format = DEVICE_FORMAT;
 					oDeviceConfig.playback.channels = 1;
@@ -154,14 +148,12 @@ void SoundManager::Init()
 	if( ma_device_init( NULL,&oDeviceConfig,&m_oDevice ) != MA_SUCCESS )
 	{
 		std::cerr << "SOUNDMANAGER::FAILED_TO_INITIALIZE_DEVICE" << std::endl;
-		ma_audio_buffer_uninit( &g_oAudioBuffer );
 		ma_waveform_uninit( &g_oWaveForm );
 		return;
 	}
 	if( ma_device_start( &m_oDevice ) != MA_SUCCESS )
 	{
 		std::cerr << "SOUNDMANAGER::FAILED_TO_START_DEVICE" << std::endl;
-		ma_audio_buffer_uninit( &g_oAudioBuffer );
 		ma_waveform_uninit( &g_oWaveForm );
 		ma_device_uninit( &m_oDevice );
 		return;
