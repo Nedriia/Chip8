9233a9faaf6453d2c5c702778f298560149bf6c8
Nedriia <arthur.favre@hotmail.com>
2026-09-16 17:56:18 +0200

    First commit for hexEditor integration

--- Chip8_Emulation/Display.h
+++ Chip8_Emulation/Display.h
@@ -4,6 +4,8 @@
 #include "Shader.h"
 #include <chrono>
 
+#include "HexEditor/src/HexEditor_ImGUI.h"
+
 #define DEBUG_INFO
 
 enum ResolutionMode
@@ -103,4 +105,6 @@ protected:
 	ResolutionMode						m_oResolutionMode;
 	PlaneBitMask						m_oCurrentBitMask;
 	static uint8_t						m_iBitPlaneDrawIteration;
+	static HexEditor_ImGUI oEditor;
+
 };
