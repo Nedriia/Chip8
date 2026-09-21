14a758d22d559442235fc465d8924a489691a868
Nedriia <arthur.favre@hotmail.com>
2026-09-21 10:23:45 +0200

    Update ImGui Font params

--- Chip8_Emulation/Chip8_Debugger.cpp
+++ Chip8_Emulation/Chip8_Debugger.cpp
@@ -86,10 +86,12 @@ void Chip8_Debugger::Init( GLFWwindow* mainWindow,const Chip8* pCPU )
 	ImGui::StyleColorsClassic();
 	//ImGui::StyleColorsLight();
 
-	// Setup scaling
 	ImGuiStyle& style = ImGui::GetStyle();
-	style.ScaleAllSizes( main_scale );        // Bake a fixed style scale. (until we have a solution for dynamic style scaling, changing this requires resetting Style + calling this again)
-	style.FontScaleDpi = main_scale;        // Set initial font scale. (using io.ConfigDpiScaleFonts=true makes this unnecessary. We leave both here for documentation purpose)
+	float fMain_scale = 1.0f;
+	style.FontSizeBase = 15.0f;
+
+	style.ScaleAllSizes( fMain_scale );        // Bake a fixed style scale. (until we have a solution for dynamic style scaling, changing this requires resetting Style + calling this again)
+	style.FontScaleDpi = fMain_scale;        // Set initial font scale. (using io.ConfigDpiScaleFonts=true makes this unnecessary. We leave both here for documentation purpose)
 
 	// Setup Platform/Renderer backends
 	ImGui_ImplGlfw_InitForOpenGL( mainWindow,true );
